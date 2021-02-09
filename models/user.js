6a5f20289aa1401f7e605de107c973e7287f897c
Christopher Jaeger <cjaeger.ld@gmail.com>
2021-02-09 13:17:24 -0500

    Added Password Hash

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
     class User extends Model {}
@@ -19,7 +20,11 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING
         },
         password: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            set(val) {
+                const hashedPassword = bcrypt.hashSync(val, 10);
+                this.setDataValue('password', hashedPassword);
+            }
         }
     }, { sequelize });
 
